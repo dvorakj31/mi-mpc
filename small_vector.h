1533405bb77c20c60d11d54dcda194a1625e0a58
Jakub Dvorak <dvoraj84@fit.cvut.cz>
2019-12-16 12:03:16 +0100

    constructors, assignments, implementation class

--- small_vector.h
+++ small_vector.h
@@ -1,7 +1,6 @@
 #ifndef MPC_SMALL_VECTOR
 #define MPC_SMALL_VECTOR
 
-#include <vector>
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
@@ -15,87 +14,202 @@ namespace mpc
     template <typename T, size_t N>
     class small_vector
     {
-        T* data_;
-        alignas(T) T data_buf_[N * sizeof(T)];
-        size_t capacity_;
-        size_t size_;
 
-        void _resize(size_t new_size)
-        {
-            T* data = (T*)::operator new(new_size * sizeof(T));
-            std::uninitialized_move(data_, data_ + size_, data);
-            std::destroy(data_, data_ + size_);
-            ::operator delete(data_);
-            data_ = data;
-            capacity_ = new_size;
-        }
+	public:
+		using value_type = T;
+		using reference = T&;
+		using const_reference = const T&;
+		using pointer = T*;
+		using const_pointer = const T*;
+		using iterator = T*;
+		using const_iterator = const T*;
+	private:
+		class Impl {
+			T* data_;
+			typename std::aligned_storage<sizeof(T), alignof(T)>::type data_buf_[N];
+			size_t capacity_;
+			size_t size_;
+
+			void _resize(size_t new_size)
+			{
+				T* data = (T*)::operator new(new_size * sizeof(T));
+				std::uninitialized_move(data_, data_ + size_, data);
+				std::destroy(data_, data_ + size_);
+				if (data_ != reinterpret_cast<T*>(data_buf_))
+					::operator delete(data_);
+				data_ = data;
+				capacity_ = new_size;
+			}
+
+		public:
+			constexpr Impl() : data_(reinterpret_cast<T*>(data_buf_)), capacity_(N), size_(0) {}
+			constexpr Impl(std::initializer_list<T> l) 
+			{
+				for (auto& item : l)
+					push_back(item);
+			}
+
+			Impl(const Impl& other)
+			{
+				if (other.size_ == 0) return;
+				data_ = (T*)::operator new(other.size_ * sizeof(T));
+				capacity_ = other.size_;
+				try { std::uninitialized_copy(other.data_, other.data_ + other.size_, data_); }
+				catch (...) { ::operator delete(data_); throw; }
+				size_ = other.size_;
+			}
+
+			~Impl() {
+				clear();
+				if (data_ != reinterpret_cast<T*>(data_buf_))
+					::operator delete(data_);
+			}
+
+			Impl& operator=(const Impl& rhs)
+			{
+				clear();
+				std::uninitialized_copy(rhs.data_, rhs.data_ + rhs.size_, data_);
+				size_ = rhs.size_;
+			}
+
+			constexpr size_t size() const
+			{
+				return size_;
+			}
+
+			constexpr size_t capacity() const
+			{
+				return capacity_;
+			}
+
+			constexpr void reserve(size_t new_size)
+			{
+				if (new_size <= capacity_)
+					return;
+				_resize(new_size);
+			}
+
+			void push_back(const T& value)
+			{
+				emplace_back(value);
+			}
+
+			void push_back(T&& value)
+			{
+				emplace_back(value);
+			}
+
+			T& operator[](size_t index)
+			{
+				return data_[index];
+			}
+
+			const T& operator[](size_t index) const
+			{
+				return data_[index];
+			}
+
+			template <class... _Args>
+			void emplace_back(_Args&&... args)
+			{
+				if (size_ == capacity_)
+					reserve(capacity_ == 0 ? 1 : 2 * capacity_);
+				new (data_ + size_) T(std::forward<_Args>(args)...);
+				size_++;
+			}
+
+			void clear() noexcept
+			{
+				std::destroy(data_, data_ + size_);
+				size_ = 0;
+			}
+		};
+
+		Impl* pimpl_;
 
     public:
-        constexpr small_vector() : data_(data_buf_), capacity_(N), size_(0) {}
-        constexpr small_vector(std::initializer_list<T> l) {}
-
+        constexpr small_vector() : pimpl_(new Impl()) {}
+        constexpr small_vector(std::initializer_list<T> l) : pimpl_(new Impl(l)) {}
+		small_vector(const small_vector& other) : pimpl_(new Impl(*other.pimpl_)) {}
+		small_vector(small_vector&& other) noexcept : pimpl_(other.pimpl_)
+		{			other.pimpl_ = nullptr;		}		small_vector& operator=(const small_vector& other) 
+		{
+			if (capacity() < other.size()) 
+			{ 
+				small_vector temp(other);
+				swap(temp); 
+			}
+			else 
+			{
+				if (this != &other) 
+				{
+					*pimpl_ = *other.pimpl_;
+				}
+			}
+			return *this;
+		}		small_vector& operator=(small_vector&& other) noexcept		{			pimpl_ = other.pimpl_;			other.pimpl_ = nullptr;			return *this;		}
         ~small_vector()
         {
-            if (data_ != data_buf_)
-                delete[] data_;
+			delete pimpl_;
         }
 
         constexpr size_t size() const
         {
-            return size_;
+            return pimpl_->size();
         }
 
         constexpr size_t capacity() const
         {
-            return capacity_;
+            return pimpl_->capacity();
         }
 
         constexpr void reserve(size_t new_size)
         {
-            if (new_size <= capacity_)
-                return;
-            _resize(new_size);
+			pimpl_->reserve(new_size);
         }
 
         void push_back(const T& value)
         {
-            emplace_back(value);
+            pimpl_->push_back(value);
         }
 
         void push_back(T&& value)
         {
-            emplace_back(value);
+            pimpl_->push_back(value);
         }
 
         T& operator[](size_t index)
         {
-            return data_[index];
+			return (*pimpl_)[index];
         }
 
         const T& operator[](size_t index) const
         {
-            return data_[index];
+			return (*pimpl_)[index];
         }
 
         template <class... _Args>
         void emplace_back(_Args&&... args)
         {
-            if (size_ == capacity_)
-                reserve(capacity_ == 0 ? 1 : 2 * capacity_);
-            new (data_ + size_) T(std::forward<_Args>(args)...);
-            size_++;
+			pimpl_->emplace_back(std::forward<_Args>(args)...);
         }
 
         void clear() noexcept
         {
-            if (data_ != data_buf_)
-            {
-                std::destroy(data_, data_ + size_);
-                ::operator delete(data_);
-            }
-            size_ = 0;
+			pimpl_->clear();
         }
+
+		void swap(small_vector& rhs) noexcept
+		{
+			std::swap(pimpl_, rhs.pimpl_);
+		}
     }; // class small vector
+
+	template <typename T, size_t N>
+	void swap(small_vector<T, N>& lhs, small_vector<T, N>& rhs) noexcept
+	{
+		lhs.swap(rhs);
+	}
 
 } // namespace mpc
 
